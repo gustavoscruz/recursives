892bfb0637ed6944aa9095790d3d2f76721b255a
gustavoscruz <gustadscruz@gmail.com>
2023-11-12 09:22:37 -0300

    Created DigitCount Class

--- src/recursives_1/Palindrome.java
+++ src/recursives_1/Palindrome.java
@@ -2,12 +2,11 @@ package recursives_1;
 
 public class Palindrome {
 
-    public static String palindrome(String sentence){
-        if(sentence.length() == 20){
-
-        }
-        else{
-
+    public static String palindrome(String sentence) {
+        if (sentence.length() == 20) {
+            return "k";
+        } else {
+            return "ku";
         }
     }
 
