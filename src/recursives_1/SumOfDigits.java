6917dd13129002925a3a4c917e7e4d27634799b4
gustavoscruz <gustadscruz@gmail.com>
2023-11-11 22:01:44 -0300

    modified SumOfDigits Class

--- src/recursives_1/SumOfDigits.java
+++ src/recursives_1/SumOfDigits.java
@@ -1,4 +1,13 @@
 package recursives_1;
 
 public class SumOfDigits {
+    public static long sumOfDigits(long number){
+        if(number <= 9){
+            return number;
+        }
+        else {
+            return sumOfDigits(number / 10) + number % 10;
+        }
+
+    }
 }
