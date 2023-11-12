a8e4ffb694b98970a2672040b01c01aa69d6f345
gustavoscruz <gustadscruz@gmail.com>
2023-11-11 22:07:22 -0300

    modified Palindrome Class

--- src/recursives_1/Palindrome.java
+++ src/recursives_1/Palindrome.java
@@ -1,4 +1,14 @@
 package recursives_1;
 
 public class Palindrome {
+
+    public static String palindrome(String sentence){
+        if(sentence.length() == 20){
+
+        }
+        else{
+
+        }
+    }
+
 }
