c7c6bd337abb4116091df3a0b122653962ec11d9
gustavoscruz <gustadscruz@gmail.com>
2023-11-11 22:08:11 -0300

    modified RecursiveTest Class

--- src/recursives_1/RecursiveTest.java
+++ src/recursives_1/RecursiveTest.java
@@ -2,6 +2,6 @@ package recursives_1;
 
 public class RecursiveTest {
     public static void main(String[] args){
-        System.out.println(SumOfDigits.sumOfDigits(256));
+        System.out.println(Palindrome.palindrome("SSSS"));
     }
 }
