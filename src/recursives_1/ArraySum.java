91677c5995a541a0bb19ed9839c86bc44bcf5f1b
gustavoscruz <gustadscruz@gmail.com>
2023-11-12 11:36:43 -0300

    Updated ArraySum Class

--- src/recursives_1/ArraySum.java
+++ src/recursives_1/ArraySum.java
@@ -1,4 +1,15 @@
 package recursives_1;
 
 public class ArraySum {
+
+    public static long arraySum(int[] array, int tam){
+
+        if(tam == 0){
+            return 0;
+        } else{
+            return array[tam - 1] + arraySum(array, tam - 1);
+        }
+
+    }
+
 }
--- src/recursives_1/RecursiveTest.java
+++ src/recursives_1/RecursiveTest.java
@@ -2,6 +2,7 @@ package recursives_1;
 
 public class RecursiveTest {
     public static void main(String[] args){
-        System.out.println(DigitCount.digitCount(0));
+        int[] array = {1, 2, 3};
+        System.out.println(ArraySum.arraySum(array, array.length));
     }
 }
