3cae8e1c17f8f30490953fa720a6f4ec6fc94c0c
gustavoscruz <gustadscruz@gmail.com>
2023-11-12 09:25:14 -0300

    Updated DigitCount Class

--- src/recursives_1/DigiteCount.java
+++ src/recursives_1/DigiteCount.java
@@ -1,4 +1,13 @@
 package recursives_1;
 
 public class DigiteCount {
+
+    public static int digitCount(long number){
+        if(number <= 9){
+            return 1;
+        }
+        else {
+            return digitCount(number / 10) + 1;
+        }
+    }
 }
--- src/recursives_1/RecursiveTest.java
+++ src/recursives_1/RecursiveTest.java
@@ -2,6 +2,6 @@ package recursives_1;
 
 public class RecursiveTest {
     public static void main(String[] args){
-        System.out.println(Palindrome.palindrome("SSSS"));
+        System.out.println(DigiteCount.digitCount(0));
     }
 }
