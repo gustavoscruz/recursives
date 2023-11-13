e535c73f442ddff6a19cb4b0f71de52b2a76d81f
gustavoscruz <gustadscruz@gmail.com>
2023-11-12 21:17:07 -0300

    Modified Palindrome Class

--- src/recursives_1/RecursiveTest.java
+++ src/recursives_1/RecursiveTest.java
@@ -3,6 +3,6 @@ package recursives_1;
 public class RecursiveTest {
     public static void main(String[] args){
         int[] array = {1, 2, 3};
-        System.out.println();
+        System.out.println(Palindrome.palindrome("subinoonibus", "subinoonibus".length()));
     }
 }
